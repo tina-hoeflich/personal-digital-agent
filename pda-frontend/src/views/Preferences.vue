ab1bdc2ba4a475b3c4db01db42b84a645eeaaf3f
phlyp <pypruessner@gmail.com>
2023-03-20 11:29:30 +0100

    added preferences page

---
+++ pda-frontend/src/views/Preferences.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-theme-provider theme="dark">
+        <v-card title="Personal Info"></v-card>
+        <v-card title="Navigation Settings"></v-card>
+        <v-card title="Important Contacts"></v-card>
+        <v-card title="Financials"></v-card>
+    </v-theme-provider>
+</template>
+  
+<script>
+  //
+</script>
+
+<style>
+v-card {
+    max-width: 100px;
+}
+</style>
