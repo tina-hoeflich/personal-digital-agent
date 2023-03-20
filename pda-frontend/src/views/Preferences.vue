a5ec25934cb9a31758bf111a29f6a132f0e994b2
Phlyp <pypruessner@gmail.com>
2023-03-20 23:22:23 +0100

    basic layout

--- pda-frontend/src/views/Preferences.vue
+++ pda-frontend/src/views/Preferences.vue
@@ -1,18 +1,39 @@
 <template>
-    <v-theme-provider theme="dark">
-        <v-card title="Personal Info"></v-card>
-        <v-card title="Navigation Settings"></v-card>
-        <v-card title="Important Contacts"></v-card>
-        <v-card title="Financials"></v-card>
-    </v-theme-provider>
+  <v-theme-provider theme="dark">
+    <v-container fluid class="d-flex flex-lg-row flex-column justify-center">
+      <v-card 
+        v-for="pane in settings"
+        class="ma-2"
+        style="flex: 1"
+      >
+      <v-card-item>
+        <v-card-title class="float-left">
+          {{ pane.title }}
+        </v-card-title>
+      </v-card-item>
+      <v-card-text class="float-left">
+        This is example Text
+      </v-card-text>
+      </v-card>
+    </v-container>
+  </v-theme-provider>
 </template>
   
 <script>
-  //
+export default {
+  data() {
+    return {
+      settings: [
+        { title: "Personal Info"},
+        { title: "Navigation Settings"},
+        { title: "Important Contacts"},
+        { title: "Financials"},
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-v-card {
-    max-width: 100px;
-}
+
 </style>
