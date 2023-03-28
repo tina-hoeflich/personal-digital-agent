38f6eba0a25e403b9642455beadc768063bea8b7
tina-hoeflich <tina.h.hoeflich@gmail.com>
2023-03-28 17:55:59 +0200

    add jokes api

---
+++ pda-backend/services/jokes.py
@@ -0,0 +1,13 @@
+from jokeapi import Jokes 
+import asyncio
+
+async def print_joke():
+    j = await Jokes()  # Initialise the class
+    joke = await j.get_joke()  # Retrieve a random joke
+    if joke["type"] == "single": # Print the joke
+        print(joke["joke"])
+    else:
+        print(joke["setup"])
+        print(joke["delivery"])
+
+asyncio.run(print_joke())
