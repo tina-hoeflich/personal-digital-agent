026ec00f7fd61d47ccb3f9f177a36b4967f3baf5
David Ullmer <davidullmer@outlook.de>
2023-04-06 14:29:48 +0200

    Add email tests

--- pda-backend/services/email_service.py
+++ pda-backend/services/email_service.py
@@ -3,19 +3,13 @@ import os
 from sendgrid.helpers.mail import Mail, Email, To, Content
 
 
-
-# Send an HTTP POST request to /mail/send
-def send_email():
-    api_key = os.environ.get('SENDGRID_API_KEY')
-    api_key = os.environ.get('SENDGRID_API_KEY')
-    sg = sendgrid.SendGridAPIClient(api_key)
-    from_email = Email("jarvis@tinahoeflich.de")  # Change to your verified sender
-    to_email = To("tina.h.hoeflich@gmail.com")  # Change to your recipient
-    subject = "Jarvis asking for your support"
-    content = Content("text/plain", "Hi there, \n \nyour friend may need someone to cheer him up :) \nCan you help me out with this? \n \nThanks, \n Jarvis")
+def send_email(from_email: str, to_email: str, subject: str, content: str) -> Mail:
+    sg = sendgrid.SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email(from_email)
+    to_email = To(to_email)
+    subject = subject
+    content = Content("text/plain", content)
     mail = Mail(from_email, to_email, subject, content)
 
-    # Get a JSON-ready representation of the Mail object
-    mail_json = mail.get()
-    response = sg.client.mail.send.post(request_body=mail_json)
-    print(response.status_code)
+    sg.send(mail)
+    return mail
