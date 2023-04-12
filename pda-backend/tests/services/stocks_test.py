9492c787ba1fa36f6582941a4e0f9c8a570ccdcd
David Ullmer <davidullmer@outlook.de>
2023-04-12 11:20:34 +0200

    Fix tests?

--- pda-backend/tests/services/stocks_test.py
+++ pda-backend/tests/services/stocks_test.py
@@ -1,7 +1,10 @@
+import os
+
 import services.stocks as stocks
 
 
 def test_get_stocks():
+	os.environ["ALPHAVANTAGE_API_KEY"] = "5MECCCPWG3K5D69X"
 	price = stocks.get_stock_price("MSFT")
 	assert type(price) == float
 	assert price > 10
