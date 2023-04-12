71a8ea800cf8da207759e51ce345d2a452b45ec1
David Ullmer <davidullmer@outlook.de>
2023-04-12 11:32:49 +0200

    Fix tests now?

--- pda-backend/services/tankerkoenig.py
+++ pda-backend/services/tankerkoenig.py
@@ -2,8 +2,6 @@ import os
 
 import pytankerkoenig as tankerkoenig
 
-APIKEY = os.environ.get("TANKERKOENIG_API_KEY")
-
 
 def get_fuelprice(fuel_type: str, lat: float, long: float, radius: int) -> tuple[str, float]:
 	"""Get the lowest current fuel price for a given fuel type at the current location in a defined radius.
@@ -18,7 +16,7 @@ def get_fuelprice(fuel_type: str, lat: float, long: float, radius: int) -> tuple
 		str: the location of the fuel station with the lowest radius
 		float: the lowest fuel price in the defined radius
 	"""
-	stationsInRadius = tankerkoenig.getNearbyStations(APIKEY, str(lat), str(long), str(radius), fuel_type, "price")
+	stationsInRadius = tankerkoenig.getNearbyStations(os.environ.get("TANKERKOENIG_API_KEY"), str(lat), str(long), str(radius), fuel_type, "price")
 	bestPrice = stationsInRadius["stations"][0]["price"]
 	location = stationsInRadius["stations"][0]["name"] + ", " + stationsInRadius["stations"][0]["street"] + ", " + stationsInRadius["stations"][0]["place"]
 	return location, bestPrice
--- pda-backend/tests/services/tankerkoenic_test.py
+++ pda-backend/tests/services/tankerkoenic_test.py
@@ -1,7 +1,10 @@
+import os
+
 import services.tankerkoenig as tankerkoenig
 
 
 def test_get_fuelprice():
+	os.environ["TANKERKOENIG_API_KEY"] = "5764c28e-6601-cf68-01ec-c81ccc04eec4"
 	location, price = tankerkoenig.get_fuelprice("e10", 48.78486972213759, 9.181908387899563, 5)
 	assert type(location) == str
 	assert type(price) == float
