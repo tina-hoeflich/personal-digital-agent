5999a854e5249d3da2d2a80fef238523756aac85
florian.strehlke <florian.strehlke@dhbw-engineering.de>
2023-04-19 01:59:19 +0200

    added first event method for philip

--- pda-backend/services/calender.py
+++ pda-backend/services/calender.py
@@ -1,11 +1,11 @@
 import icalendar
 from datetime import datetime, date, timedelta
 
-def get_last_event() -> tuple[str, str, str]:
+def get_last_event() -> tuple[str, datetime.time, datetime.time]:
     """This method gets the last event of the day from the student calender.
 
     Returns:
-        tuple[str, str, str]: name of last event, end time of last event, time to watch movie
+        tuple[str, datetime.time, datetime.time]: name of last event, end time of last event, time to watch movie
     """    
     date_today = date.today()
 
@@ -29,6 +29,39 @@ def get_last_event() -> tuple[str, str, str]:
     if last_event_end_time is not None:
         last_event_end_time = last_event_end_time.time()
         new_time = (datetime.combine(date_today, last_event_end_time) + timedelta(hours=1, minutes=30)).time()
-        return last_event_name, str(last_event_end_time), str(new_time)
+        return last_event_name, last_event_end_time, new_time
     else:
         return None, None, None
+    
+from datetime import date, datetime, timedelta
+import icalendar
+
+def get_first_event_start_time() -> tuple[str, datetime.time]:
+    """This method gets the start time of the first event of the day from the student calendar.
+
+    Returns:
+        tuple[str, str, datetime.time]: name of first event, start time of first event
+    """
+    date_today = date.today()
+
+    with open(r'resources\student_calender.ics', 'rb') as f:
+        calendar = icalendar.Calendar.from_ical(f.read())
+
+    first_event_start_time = None
+
+    for component in calendar.walk():
+        if component.name == "VEVENT":
+            event = {}
+            event["summary"] = str(component.get('summary'))
+            event["start_time"] = component.get('dtstart').dt
+            event["end_time"] = component.get('dtend').dt
+
+            if event["start_time"].date() == date_today:
+                if first_event_start_time is None or event["start_time"].time() < first_event_start_time.time():
+                    first_event_start_time = event["start_time"]
+                    first_event_name = event["summary"]
+
+    if first_event_start_time is not None:
+        return first_event_name, first_event_start_time.time()
+    else:
+        return None, None
--- pda-backend/tests/services/calender_test.py
+++ pda-backend/tests/services/calender_test.py
@@ -1,8 +1,15 @@
 import services.calender as calender
+from datetime import time
 
 def test_get_last_event(): 
     last_event_name, last_event_end_time, new_time = calender.get_last_event()
     
     assert type(last_event_name) == str or last_event_name == None
-    assert type(last_event_end_time) == str or last_event_end_time == None
-    assert type(new_time) == str or new_time == None
+    assert isinstance(last_event_end_time, time) or last_event_end_time == None
+    assert isinstance(new_time, time) or new_time == None
+
+def test_get_first_event_start_time(): 
+    first_event_name, first_event_start_time = calender.get_first_event_start_time()
+    
+    assert type(first_event_name) == str or first_event_name == None
+    assert isinstance(first_event_start_time, time) or first_event_start_time == None
