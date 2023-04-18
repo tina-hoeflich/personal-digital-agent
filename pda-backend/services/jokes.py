8f31a12faef090376d9736512081cc78ba6cd267
tina-hoeflich <tina.h.hoeflich@gmail.com>
2023-04-19 00:32:41 +0200

    blacklist jokes and add env secret

--- pda-backend/services/jokes.py
+++ pda-backend/services/jokes.py
@@ -3,7 +3,7 @@ from jokeapi import Jokes
 
 async def get_joke():
     j = await Jokes()
-    joke = await j.get_joke()
+    joke = await j.get_joke(blacklist=["nsfw", "religious", "political", "racist", "sexist"])  # Retrieve a random joke without nsfw, religious, political, racist, sexist jokes
     if joke["type"] == "single":
         text = joke["joke"]
     else:
